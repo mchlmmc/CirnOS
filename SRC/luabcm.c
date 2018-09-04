334b86c53a95f4423fe1a781aa9e1e0b3c5ad301
Michael Mamic <michael.mamic78@gmail.com>
2018-09-03 21:12:10 -0700

    Add wrappers for readPin
    Userspace Lua code can now read the rPi's
    pins using readPin.
    
    Constants on and off have been renamed
    ON and OFF to stay consistent with Lua's
    standard code style.
    
    Additional minor tweaks have been made in
    luabcm.c and luabcm.h, including header
    guards and edits for conciseness.

--- SRC/luabcm.c
+++ SRC/luabcm.c
@@ -1,3 +1,19 @@
+// CirnOS -- Minimalistic scripting environment for the Raspberry Pi
+// Copyright (C) 2018 Michael Mamic
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
 #include "bcm2835.h"
 #include "stdio.h"
 #include "LUA/lua.h"
@@ -20,19 +36,6 @@ static int l_fsel (lua_State *L) {
   }
   
   uint8_t pin = (uint8_t)p;
-  switch (pin) {
-  case 6:
-  case 9:
-  case 14:
-  case 20:
-  case 25:
-  case 30:
-  case 34:
-  case 39:
-    luaL_error(L, "BCM2835 Error: Invalid GPIO pin number.");        
-  default:
-    break;
-  }
 
   double k = luaL_checknumber(L, 2);  
   if((double)(uint8_t)k != k) {
@@ -55,32 +58,51 @@ static int l_write (lua_State *L) {
   }
   
   uint8_t pin = (uint8_t)p;
-  switch (pin) {
-  case 6:
-  case 9:
-  case 14:
-  case 20:
-  case 25:
-  case 30:
-  case 34:
-  case 39:
-    luaL_error(L, "BCM2835 Error: Invalid GPIO pin number.");        
+
+  int k = 0;
+
+  switch(lua_type(L, 2)) {
+  case LUA_TBOOLEAN:
+    k = lua_toboolean(L, 2);
+    break;
+  case LUA_TNUMBER:
+    k = lua_tonumber(L, 2);
+    break;
   default:
+    luaL_error(L, "BCM2835 Error: Invalid type for status (expected boolean or number).");    
     break;
   }
 
-  if(lua_type(L, 2) != LUA_TBOOLEAN) {
-    luaL_error(L, "BCM2835 Error: Invalid type for status (expected boolean).");
-  }
+  // LED on Pi Zero
+  if(pin == 47) k = !k;
 
-  int k = lua_toboolean(L, 2);
-
-  if(k) bcm2835_gpio_clr(pin);
-  else bcm2835_gpio_set(pin);
+  bcm2835_gpio_write(pin, k);
   
   return 0;
 }
 
+static int l_lev (lua_State *L) {
+  double p = luaL_checknumber(L, 1);
+  if((double)(uint8_t)p != p) {
+    luaL_error(L, "BCM2835 Error: Invalid argument for pin (expected uint32_t).");
+  }
+  
+  uint8_t pin = (uint8_t)p;
+
+  lua_pushboolean(L, bcm2835_gpio_lev(pin));
+  
+  return 1;
+}
+
+/**
+ * luabcm_register - Adds BCM library to Lua
+ *
+ * @L: Lua environment to add to
+ *
+ * Registers various constants and 
+ * functions to allow Lua user code to
+ * access the rPi's peripherals.t
+ */
 void luabcm_register(lua_State *L) {
   lua_pushcfunction(L, l_delay);
   lua_setglobal(L, "delay");
@@ -88,12 +110,15 @@ void luabcm_register(lua_State *L) {
   lua_setglobal(L, "pinMode");
   lua_pushcfunction(L, l_write);
   lua_setglobal(L, "writePin");
+  lua_pushcfunction(L, l_lev);
+  lua_setglobal(L, "readPin");
+  
   lua_pushboolean(L, 1);
-  lua_setglobal(L, "on");
+  lua_setglobal(L, "ON");
   lua_pushboolean(L, 0);
-  lua_setglobal(L, "off");
-  lua_pushnumber(L, 0);
-  lua_setglobal(L, "input");
+  lua_setglobal(L, "OFF");
+  lua_pushnumber(L, 0);  
+  lua_setglobal(L, "INPUT");
   lua_pushnumber(L, 1);
-  lua_setglobal(L, "output");      
+  lua_setglobal(L, "OUTPUT");      
 }
